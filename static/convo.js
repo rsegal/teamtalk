4caec044cb4d855479b9a9bbcba455fad7c5963c
Raphael Segal <rsegal@andrew.cmu.edu>
2013-04-11 19:22:05 -0400

    additional work on demo

--- static/convo.js
+++ static/convo.js
@@ -3,11 +3,23 @@ function connect() {
 }
 
 function changeUser() {
-	
-}
-
-function main() {
 
 }
 
-main();
+$('header').click(function(event) {
+
+});
+
+window.onresize = function(event) {
+    resizeDiv();
+}	
+
+function resizeDiv() {
+	console.log("Resizing height");
+    h = $(window).height(); 
+    $('body').css({'height': h + 'px'});
+}
+
+$(document).ready(function(){
+    resizeDiv();
+});
